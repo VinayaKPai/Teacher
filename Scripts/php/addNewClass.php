131b0027bdd4b433fe5f31114e162964b4f1d725
Vinaya <vinayakeshavpai@gmail.com>
2019-06-16 10:31:43 +0530

    Unknown

---
+++ Scripts/php/addNewClass.php
@@ -0,0 +1,8 @@
+<?php include ".../basecode-create_connection.php";
+	echo $datetime1; 
+
+echo "Individual echo below";
+$classNumber = $_POST["classNumber"];
+echo $classNumber;
+//echo $_POST["sectionAlpha"]:
+?>
